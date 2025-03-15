a8b841c22b83ee37151d9894e4f2c8fc210cfe7b
sivaprakashgorantla <sivaprakashgorantla@gmail.com>
2025-03-15 11:02:05 +0530

    updated

--- src/shoppingfolder/components/Collections.jsx
+++ src/shoppingfolder/components/Collections.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Collections = (props) => {
 
     console.log(props.gentsFashion);
-    const { title,image1, image2,image3,image4,image5,image6, image7,image8,image9} = props.gentsFashion;
+    const { title,image1, image2,image3,image4,image5,image6, image7,image8} = props.gentsFashion;
     return (
         <div className="collectionSection">
             <h2>{title}</h2>
+            <img src="assets/Men/GentsBanner.gif" alt={title} style={{ width: "100vw",  objectFit: "cover" }} />
             <div className="menImages ">
                 <img src={image1} alt={title} />
                 <img src={image2} alt={title} />
